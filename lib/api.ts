5b7a9a039f5d535d65779cd7f0ac0639cf62e528
Perumpuli Arachchilage Yasindu Anushka Gunasekara <yasindugunasekara774@gmail.com>
2026-02-20 00:00:59 +0530

    Change local backend url

--- lib/api.ts
+++ lib/api.ts
@@ -1,1 +1,8 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
+import axios from 'axios';
+ 
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+});
+ 
+export default api;
